b7b401cfb3c3eb7db0b02f387bc99c66e823cec9
Viridiana Leal <lerv060816mhglmra8@soycecytem.mx>
2023-04-28 09:08:16 -0500

    first commit

---
+++ 01 hola/main.js
@@ -0,0 +1,12 @@
+
+const hola=()=>{
+  alert("hola mundo ");
+  let nombre=prompt("cual es tu nombre ");
+  alert("Hola "+nombre+" 🤗");
+}
+const suma=()=>{
+  let nm1=parseInt(prompt("Número 1:"));
+  let nm2=parseInt(prompt("Número 2:"));
+  let suma=(nm1+nm2);
+  alert("El resultado es:"+suma);
+}
---
+++ 03 slider/app.js
@@ -0,0 +1,4 @@
+const valor =()=>{
+  
+  document.querySelector("#res").innerHTML=document.querySelector("#v").value;
+}
